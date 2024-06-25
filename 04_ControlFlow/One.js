a1d3c1e3d8d6a7b5ea0bdb8031261be19ddaed11
Roman-Khadka0 <romankhadkawork@gmail.com>
2024-06-25 15:02:37 +0000

    ControlFlow Completed

---
+++ 04_ControlFlow/One.js
@@ -0,0 +1,53 @@
+//if 
+const isUserLoggedin = false
+if(isUserLoggedin){
+    //console.log("yes,Roman is logged in")
+}
+else{
+   // console.log("No,Roman is not logged in ");
+}
+
+if(2==="2"){
+    //console.log("Executed");
+}
+
+// <,>,<=,>=,==,!=,===,!==
+
+const temp =  52
+if(temp===51){
+   // console.log("Tempreature is less than 51");
+}
+else{
+    //console.log("Trmpreture is greater than 51 degree")
+}
+
+const score = 200
+if(score>100){
+    const power = "Unlimited health"
+    //console.log(` Userpower: ${power}`);
+}
+
+
+/*const balance = 1000
+//if (balance > 500); console.log("Balance greater than 500"),
+//console.log("current balance:",balance);
+if(balance<500){
+    console.log("less than");
+}
+else if(balance <750){
+    console.log("less than 750");
+}
+else if (balance < 900){
+    console.log("less than 900");
+}
+else{
+    console.log("less than 1200");
+}
+*/
+
+const isLoggedIn = true
+const debitCard = true
+
+if(isUserLoggedin && debitCard){
+    console.log("purchase Successfull");
+}
