eafef5ffb3e830e89de7be2a5307206e08e19802
Roman-Khadka0 <romankhadkawork@gmail.com>
2024-06-26 16:14:08 +0000

    Break And Continue Completed

---
+++ 05_Iteration/Break_Continue.js
@@ -0,0 +1,28 @@
+// Break and Continue
+
+//for (let index = 1; index <= 20; index++) {
+  //  if(index == 5) {
+    //    console.log("5 detected");
+      //  break
+    //}
+    //console.log(index);
+    
+//}
+//for (let index = 1; index <= 20; index++) {
+  //  if(index == 5) {
+    //    console.log("5 detected");
+    //    continue
+    //}
+  //  console.log(index);
+    
+//}
+
+myArray = ["Roman","Ram","Nani","Sumanta","Rashmita","Susma"]
+for (let index = 0; index < myArray.length; index++){
+    const element = myArray[index]
+    if(myArray == "Roman"){
+        console.log("Roman Found");
+        break
+    }
+
+}
