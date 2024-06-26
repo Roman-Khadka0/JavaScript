9e02444e458d8a36c9d214631dd0a92d0296882e
Roman-Khadka0 <romankhadkawork@gmail.com>
2024-06-26 16:58:29 +0000

    Do While Completed

---
+++ 05_Iteration/do.js
@@ -0,0 +1,19 @@
+//let index = 0
+//while (index<=10) {
+    //console.log(index);
+  //  index = index+2
+//}
+
+//let array =["Roman","Ram","Nani"]
+//let arr = 0 
+//while(arr<array.length){
+  //  console.log(` ${array[arr]}`);
+    //arr=arr+1
+    
+//}
+let score = 1
+do{
+    console.log("score is",score)
+     score++
+}
+while(score<=10)
