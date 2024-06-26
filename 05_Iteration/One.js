bcfc252668f3590d8f5046c320de6bad2ea28bf1
Roman-Khadka0 <romankhadkawork@gmail.com>
2024-06-26 16:01:28 +0000

    For Loop Completed

---
+++ 05_Iteration/One.js
@@ -0,0 +1,25 @@
+//for
+
+//for (let i = 0; i < 10; j++) {
+   // const element = index;
+    //console.log(element);
+   // if (element == 9) {
+        //console.log("best number");
+  //  }
+//}
+for (let i = 0; i <= 10 ; i++) {
+    //console.log(`Outer loop value ${i}`);
+    for (let j = 0; j <= 10; j++) {
+    //console.log(`inner loop value ${i} and ${j}`);
+   // console.log(i + '*' + j +'=' + i*j);        
+    }
+    
+}
+
+let array = ["Roman","Ram","Nani","Sumanta","Rashmita","Susma"]
+//console.log(array.length);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+ //   console.log(element)
+}
+
