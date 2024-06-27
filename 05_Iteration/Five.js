71c8402dae0db39d4188b21f33666704fad8379f
Roman-Khadka0 <romankhadkawork@gmail.com>
2024-06-27 09:23:41 +0000

    ForIN loop completed

---
+++ 05_Iteration/Five.js
@@ -0,0 +1,15 @@
+const myObj = {
+    js:"JavaScript",
+    cpp: "C++",
+    rb: "Ruby"
+}
+ for (const key in myObj) {
+    //console.log(`${key} is shortcut of ${myObj[key]}`);    
+ }
+
+ const prog = ["js","ruby","py","java"]
+
+ for (const key in prog) {
+   console.log(key);    
+    }
+ 
